694b4f806846fd46202bdfcf550757cbfa936897
iklymchuk <klimchuk_ivan@mail.ru>
2015-02-16 19:41:00 +0200

    fix interceptor

--- JBC_mvc/src/main/java/ru/javabegin/training/interceptors/CheckUserInterceptor.java
+++ JBC_mvc/src/main/java/ru/javabegin/training/interceptors/CheckUserInterceptor.java
@@ -1,8 +1,11 @@
 package ru.javabegin.training.interceptors;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import ru.javabegin.training.objects.User;
@@ -10,15 +13,16 @@ import ru.javabegin.training.objects.User;
 public class CheckUserInterceptor extends HandlerInterceptorAdapter {
 	
 	@Override
-	public void afterCompletion (HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	public void postHandle (HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws IOException {
+		
 		if (request.getRequestURI().contains("check-user")) {
-			
-			User user = (User) request.getAttribute("user");
-			if (user == null || !user.isAdmin()) {
-				response.sendRedirect(request.getContextPath() + "/failed");
-			}
-			
+					
+					User user = (User) modelAndView.getModel().get("user");
+					if (user == null || !user.isAdmin()) {
+						response.sendRedirect(request.getContextPath() + "/failed");
+					}
+					
+				}
 		}
-	}
 
 }
